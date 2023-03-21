a67cf8876129786c0080d685c7e536d7fed960dd
Maurice Zhou <jasper@apvc.uk>
2023-03-21 10:40:48 +0100

    init
    Signed-off-by: Maurice Zhou <jasper@apvc.uk>

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Build GRUB rescue image";
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.grubrescue = (
+        let
+            system = "x86_64-linux";
+            pkgs = nixpkgs.legacyPackages.${system};
+        in
+          pkgs.stdenv.mkDerivation {
+              name = "grubrescue";
+              buildInputs = with pkgs; [
+                grub2 grub2_efi xorriso
+                  ];
+              phases = ["buildPhase"];
+              buildPhase = ''
+              mkdir -p $out
+              ${pkgs.grub2}/bin/grub-mkrescue --output $out/grub-rescue-i386-pc.img
+              ${pkgs.grub2_efi}/bin/grub-mkrescue --output $out/grub-rescue-x86_64-efi.img
+              '';
+          }
+        );
+
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.grubrescue;
+  };
+}
