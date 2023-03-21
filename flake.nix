f294388e84d3364b7a20bd22ad02db70975bec73
Maurice Zhou <jasper@apvc.uk>
2023-03-21 17:16:05 +0100

    add grub menu
    Signed-off-by: Maurice Zhou <jasper@apvc.uk>

--- flake.nix
+++ flake.nix
@@ -1,30 +1,24 @@
 {
   description = "Build GRUB rescue image";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11"; };
 
   outputs = { self, nixpkgs }: {
 
-    packages.x86_64-linux.grubrescue = (
-        let
-            system = "x86_64-linux";
-            pkgs = nixpkgs.legacyPackages.${system};
-        in
-          pkgs.stdenv.mkDerivation {
-              name = "grubrescue";
-              buildInputs = with pkgs; [
-                grub2 grub2_efi xorriso mtools
-                  ];
-              phases = ["buildPhase"];
-              buildPhase = ''
-              mkdir -p $out
-              ${pkgs.grub2}/bin/grub-mkrescue --output $out/grub-rescue-i386-pc.img
-              ${pkgs.grub2_efi}/bin/grub-mkrescue --output $out/grub-rescue-x86_64-efi.img
-              '';
-          }
-        );
+    packages.x86_64-linux.grubrescue = (let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in pkgs.stdenv.mkDerivation {
+      name = "grubrescue";
+      src = ./.;
+      buildInputs = with pkgs; [ grub2 grub2_efi xorriso mtools ];
+      phases = [ "unpackPhase" "buildPhase" ];
+      buildPhase = ''
+        mkdir -p $out
+        ${pkgs.grub2}/bin/grub-mkrescue --output $out/grub-rescue-i386-pc.img iso
+        ${pkgs.grub2_efi}/bin/grub-mkrescue --output $out/grub-rescue-x86_64-efi.img iso
+      '';
+    });
 
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.grubrescue;
   };
