352f312ab0de60a6df208772b854d292f4f28c78
Maurice Zhou <jasper@apvc.uk>
2023-03-21 14:08:23 +0100

    flake.nix add inputs section
    Signed-off-by: Maurice Zhou <jasper@apvc.uk>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   description = "Build GRUB rescue image";
 
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+  };
+
   outputs = { self, nixpkgs }: {
 
     packages.x86_64-linux.grubrescue = (
